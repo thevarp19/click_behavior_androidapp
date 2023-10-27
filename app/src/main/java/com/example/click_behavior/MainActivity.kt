1e6403344d77ab87396a5daaedb547b05e8927ea
Yerlan Jumabayev <thevarp18@gmail.com>
2023-10-28 03:21:22 +0600

    define var's

--- app/src/main/java/com/example/click_behavior/MainActivity.kt
+++ app/src/main/java/com/example/click_behavior/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.click_behavior.ui.theme.ClickbehaviorTheme
@@ -26,7 +28,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ClickBehavior() {}
+fun ClickBehavior() {
+    var currentStep by remember { mutableStateOf(1) }
+
+    var squeezeCount by remember { mutableStateOf(0) }
+}
 
 @Preview
 @Composable
